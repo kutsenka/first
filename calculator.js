f7b6cb066f30e686148668a5bf4f9863a286b47d
AnnaMisiachna <anyamisia@gmail.com>
2023-10-30 21:39:43 +0100

    allow only numbers in Amount input

--- calculator.js
+++ calculator.js
@@ -15,3 +15,14 @@ document.getElementById("price1").onclick = function (event) {
   let result1 = DP * c * (b / 100);
   document.getElementById("result1").innerHTML = result1.toFixed(0);
 };
+
+function allowSymbols(event) {
+  const inputField = event.target;
+  // Allowing alphanumeric characters, space, and specific symbols like @, #, $
+  const allowedCharacters = /^[A-Za-z0-9\s@#$]+$/;
+
+  const inputValue = inputField.value;
+  if (!allowedCharacters.test(inputValue)) {
+    inputField.value = inputValue.slice(0, inputValue.length - 1);
+  }
+}
